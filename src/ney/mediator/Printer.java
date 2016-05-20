366b405a1e4865a29b040fcfe3ba5077d113423e
Katie Ney <akkn1995@gmail.com>
2016-05-19 21:49:10 -0400

    Router mediator code

---
+++ src/ney/mediator/Printer.java
@@ -0,0 +1,17 @@
+package ney.mediator;
+
+public class Printer implements IPoint {
+
+	@Override
+	public void recieveMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendMessage(Message message, IPoint point) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ney/observer/DiferentTypeClient.java
@@ -0,0 +1,16 @@
+package ney.observer;
+
+public class DiferentTypeClient implements IObserver {
+
+	@Override
+	public void update(Job job) {
+		processUpdate(job);
+
+	}
+
+	private void processUpdate(Job job) {
+		// do something with the job
+
+	}
+
+}
